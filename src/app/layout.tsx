9698d62d7699a62d353b8f63f245e5dac89de9d1
Popa <115492561+Popa-42@users.noreply.github.com>
2025-05-19 21:37:37 +0200

    Setup fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,19 @@ import React from "react";
 import type { Metadata } from "next";
 import "../../public/assets/styles/globals.css";
 
-import { IBM_Plex_Mono, IBM_Plex_Sans, IBM_Plex_Serif } from "next/font/google";
+import { Hind, Poppins } from "next/font/google";
 import { clsx } from "clsx";
 
-const ibmPlexSans = IBM_Plex_Sans({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
+const hind = Hind({
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
-  variable: "--font-ibm-plex-sans",
+  variable: "--font-hind",
 });
 
-const ibmPlexSerif = IBM_Plex_Serif({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
-  variable: "--font-ibm-plex-serif",
-});
-
-const ibmPlexMono = IBM_Plex_Mono({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font-ibm-plex-mono",
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
@@ -44,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <main className={clsx(ibmPlexSans.variable, ibmPlexSerif.variable, ibmPlexMono.variable, "font-sans")}>
+        <main className={clsx(hind.variable, poppins.variable, "font-sans")}>
           {children}
         </main>
       </body>
