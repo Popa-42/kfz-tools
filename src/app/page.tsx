789496650a04772e8fec144d6099745a4ebb170c
Popa <115492561+Popa-42@users.noreply.github.com>
2025-05-21 00:06:24 +0200

    Create basic randomisation tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,58 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { List } from "lucide-react";
-import csv from "csv-parser";
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import {fetchKennzeichen} from "@/lib/fetch";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   const [data, setData] = useState<Record<string, string>[]>();
+  const [currentKfz, setCurrentKfz] = useState<Record<string, string> | null>(null);
 
-  const handleButtonClick = () => {
-    const results: Record<string, string>[] = [];
-    fetch("/assets/data/de/kennzeichen.csv")
-      .then((response) => response.text())
-      .then((text) => {
-        const parser = csv();
-        parser.on("data", (row: Record<string, string>) => {
-          results.push(row);
-        });
-        parser.on("end", () => {
-          setData(results);
-        });
-        parser.write(text);
-        parser.end();
-      })
-      .catch((error) => {
-        console.error("Error fetching or parsing CSV:", error);
-      });
+  useEffect(() => {
+    fetchKennzeichen().then((r) => {
+      r.sort(() => Math.random() - 0.5);
+      setData(r);
+      setCurrentKfz(pickRandomKfz(r));
+    });
+  }, []);
+
+  const pickRandomKfz = (data: Record<string, string>[], filterState?: string): Record<string, string> | null => {
+    if (data) {
+      if (filterState) {
+        data = data.filter((item) => item["Bundesland.Iso3166-2"] === filterState);
+      }
+      data.sort(() => Math.random() - 0.5);
+      return data[0];
+    }
+    return null;
   };
 
   return (
     <div>
       <h1>Kfz-Learner</h1>
-      <p>Eine Webapp, um die deutschen Kfz-Kennzeichen zu lernen</p>
-      <Button onClick={handleButtonClick}>
-        <List />
-        Kennzeichen aufzählen
+      <Button onClick={() => setCurrentKfz(pickRandomKfz(data ?? []))} className="mb-4">
+        Randomise
       </Button>
-
-      {data && (
-        <>
-          <h2>Ergebnisse:</h2>
-          <ul>
-            {Object.entries(
-              data.reduce(
-                (acc, item) => {
-                  const bundesland = item["Bundesland.Name"] || "Unbekannt";
-                  if (!acc[bundesland]) acc[bundesland] = [];
-                  acc[bundesland].push(item);
-                  return acc;
-                },
-                {} as Record<string, Record<string, string>[]>,
-              ),
-            )
-              .sort()
-              .map(([bundesland, items]) => (
-                <section key={bundesland}>
-                  <h3>{bundesland}</h3>
-                  <ul>
-                    {items.map((item, idx) => (
-                      <li key={idx}>
-                        {item["Unterscheidungszeichen"]}: {item["StadtOderKreis"]} (
-                        {item["Herleitung"] &&
-                          [...item["Herleitung"]].map((char, i) =>
-                            /[A-ZÄÖÜẞ]/.test(char) ? <b key={i}>{char}</b> : char,
-                          )}
-                        )
-                      </li>
-                    ))}
-                  </ul>
-                </section>
-              ))}
-          </ul>
-        </>
+      <h2>Zufällige Kennzeichen:</h2>
+      {
+        data ? data.map((kfz, index) => index >= 5 ? null :(
+          <React.Fragment key={index}>
+            <h3>{kfz["Unterscheidungszeichen"]}</h3>
+            <b>{kfz["Bundesland.Name"]} </b>
+            <div>{kfz["StadtOderKreis"]}</div>
+            <div className="text-muted-foreground text-xs">
+              {kfz["HerleitungMarkiert"].split("").map((char, index) => {
+                if (/[A-ZÄÖÜẞ]/.test(char)) {
+                  return <b key={index}>{char}</b>;
+                } else {
+                  return <React.Fragment key={index}>{char}</React.Fragment>;
+                }
+              })}
+            </div>
+          </React.Fragment>
+        )
+      ) : (
+        <p>Lade...</p>
       )}
     </div>
   );
