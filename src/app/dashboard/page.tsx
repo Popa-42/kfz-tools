ab9f135b83a235f0fa80fc0b44f7088c6e0d5250
Popa <115492561+Popa-42@users.noreply.github.com>
2025-05-22 23:38:28 +0200

    Setup basic Sidebar structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-
-import React from "react";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { Separator } from "@/components/ui/separator";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -12,8 +7,10 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-export default function Home() {
+export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -21,12 +18,12 @@ export default function Home() {
         <header
           className={`
             flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear
-            group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12
+            group-has-data-[collapsible=icon]/sidebar-wrapper:h-12
           `}
         >
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
